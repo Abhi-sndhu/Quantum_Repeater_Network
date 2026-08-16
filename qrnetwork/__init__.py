8d51e8fc013c79d68ce64a96d0e1a75efb621e5e
[Abhinav Sandhu] <[sandhuabhinav55@gmail.com]>
2026-08-17 04:53:24 +0530

    Update __init__.py

--- qrnetwork/__init__.py
+++ qrnetwork/__init__.py
@@ -1,8 +1,8 @@
-from .primaryfn import Q, Qp, Is, Ebit
+from .primaryfn import Q, Qp, Is, Ebit, partial_trace, partial_trace_multi, fidelity_to_state, is_valid_density_matrix
 from .qteleportation import Teleport
 from .qchannel import QChannel
-from .network import eswap, QRep
-
+from .network import eswap, QRep, RepeaterChain, MemorySlot, EntangledPair, EntanglementSource, EntanglementSwapper, SlotStatus, heralding_statistics
+from .sim_linear import SimulationConfig, RepeaterSimulation, SimulationResult, CompletedPair
 
 # Define dagger for conjugate transpose (Hermitian adjoint) of an array or matrix
 # def dag(matrix):
